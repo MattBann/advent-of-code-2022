610ee869161aa348950ce27445ab5416ea33b40d
Matthew Bannock <69541270+MattBann@users.noreply.github.com>
2022-12-01 10:20:55 +0000

    Day 1

--- solutions/day1.py
+++ solutions/day1.py
@@ -1,3 +1,32 @@
+def run():
+    task1()
+    task2()
 
-def day1():
-    print("Hello world")
+def task1():
+    max_cal = 0
+    with open("inputs/day1task1.txt", "r") as inp:
+        cal_sum = 0
+        for line in inp:
+            if line == "\n":
+                max_cal = max(max_cal, cal_sum)
+                cal_sum = 0
+            else:
+                cal_sum += int(line)
+    print(max_cal)
+
+def task2():
+    max_cal = [0,0,0]
+    with open("inputs/day1task1.txt", "r") as inp:
+        cal_sum = 0
+        for line in inp:
+            if line == "\n":
+                max_cal.append(cal_sum)
+                max_cal.sort(reverse=True)
+                max_cal = max_cal[:3]
+                cal_sum = 0
+            else:
+                cal_sum += int(line)
+    print(sum(max_cal))
+
+if __name__ == "__main__":
+    run()
